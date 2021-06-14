746fb40af0aa6e67c01ba0a903381691c03987ee
Suhail Ahmed <71443682+SuhailAhmed2627@users.noreply.github.com>
2021-06-14 12:33:35 +0400

    Bug Fixes

--- incorpAliasAndSchedule.sh
+++ incorpAliasAndSchedule.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # run this as superuser, this script is used to incorporate aliases and make the system run schedule at 00:00, everyday
-cd ~
 echo "alias genUser='bash /home/genUser.sh'" >>.bashrc
 echo "alias permit='bash /home/permit.sh'" >>.bashrc
 echo "alias schedule='bash /home/schedule.sh'" >>.bashrc
