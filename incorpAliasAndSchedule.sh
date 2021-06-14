8dc033fc06ca01a9c6fd6872250b17e025fdec3b
Suhail Ahmed <71443682+SuhailAhmed2627@users.noreply.github.com>
2021-06-14 12:31:47 +0400

    Modified to Add Cronjobs

--- incorpAliasAndSchedule.sh
+++ incorpAliasAndSchedule.sh
@@ -1,7 +1,9 @@
-#! bin/bash
-# this script is used to incoporate the aliases into .bashrc
+#!/bin/bash
+# run this as superuser, this script is used to incorporate aliases and make the system run schedule at 00:00, everyday
 cd ~
 echo "alias genUser='bash /home/genUser.sh'" >>.bashrc
 echo "alias permit='bash /home/permit.sh'" >>.bashrc
 echo "alias schedule='bash /home/schedule.sh'" >>.bashrc
 echo "alias attendance='bash /home/attendance.sh'" >>.bashrc
+
+sudo echo "0 0 * * * root ./schedule" >>/etc/crontab
