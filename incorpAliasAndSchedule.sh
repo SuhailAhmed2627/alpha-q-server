4778eefa53f733d5257bd96b0fd45c3b4d7659bc
Suhail Ahmed <71443682+SuhailAhmed2627@users.noreply.github.com>
2021-07-26 21:17:32 +0400

    Delete incorpAliasAndSchedule.sh

--- incorpAliasAndSchedule.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# run this as superuser, this script is used to incorporate aliases and make the system run schedule at 00:00, everyday
-echo "alias genUser='bash /home/genUser.sh'" >>.bashrc
-echo "alias permit='bash /home/permit.sh'" >>.bashrc
-echo "alias schedule='bash /home/schedule.sh'" >>.bashrc
-echo "alias attendance='bash /home/attendance.sh'" >>.bashrc
-
-sudo echo "0 0 * * * root ./schedule" >>/etc/crontab
